08ae67b0e2aeb75758f91d0dbf4036d1898c001c
antonbelov <mukha19@gmail.com>
2019-11-20 19:39:38 +0300

    Доделал 4 и 5 номер

--- sql_scripts.sql
+++ sql_scripts.sql
@@ -11,4 +11,13 @@ tupdated_at = DATE_FORMAT(STR_TO_DATE(created_at,'%d.%m.%Y %H:%i'), '%Y-%m-%d %H
 -- задание номер 3
 SELECT * FROM storehouses_products ORDER BY IF(value=0, 1, 0), value;
 
+-- задание номер 4
+SELECT AVG(age) FROM tab1;
+
+-- задание номер 5 
+SELECT * FROM  persons 
+WHERE  DATE_ADD(birthday, 
+            INTERVAL YEAR(CURDATE())-YEAR(birthday)
+            + IF(DAYOFYEAR(CURDATE()) > DAYOFYEAR(birthday),1,0) YEAR)  
+            BETWEEN CURDATE() AND DATE_ADD(CURDATE(), INTERVAL 7 DAY);
 
