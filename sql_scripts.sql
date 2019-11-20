f74608075fb60bef3c13a9e05e9542e5c5d5c7eb
antonbelov <mukha19@gmail.com>
2019-11-20 14:57:58 +0300

    Окончательное дз

--- sql_scripts.sql
+++ sql_scripts.sql
@@ -5,6 +5,8 @@ UPDATE users SET tupdated_at = NOW() WHERE tupdated_at IS NULL;
 
 -- второе задание 
 created_at = DATE_FORMAT(STR_TO_DATE(created_at,'%d.%m.%Y %H:%i'), '%Y-%m-%d %H:%i:00')
+tupdated_at = DATE_FORMAT(STR_TO_DATE(created_at,'%d.%m.%Y %H:%i'), '%Y-%m-%d %H:%i:00')
+
 
 -- задание номер 3
 SELECT * FROM storehouses_products ORDER BY IF(value=0, 1, 0), value;
