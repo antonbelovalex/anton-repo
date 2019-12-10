19fa4b3e520fed7b632ffeb02d5872ee81640dfa
antonbelov <mukha19@gmail.com>
2019-12-10 13:54:16 +0300

    не до конца еще пока

--- fk_vk.sql
+++ fk_vk.sql
@@ -45,3 +45,54 @@ ALTER TABLE communities_users
 		FOREIGN KEY (friend_id) REFERENCES users(id),
 	ADD CONSTRAINT status_id_friendship_statuses_fk
 		FOREIGN KEY (status_id) REFERENCES friendship_statuses(id);
+	
+DESC likes;
+
+
+ALTER TABLE likes
+	ADD CONSTRAINT user_like_user_id_fk
+		FOREIGN KEY (user_id) REFERENCES users(id),
+	ADD CONSTRAINT target_id_media_id_fk
+		FOREIGN KEY (target_id) REFERENCES media(id),
+	ADD CONSTRAINT target_type_id_target_type_id_fk
+		FOREIGN KEY (target_type_id) REFERENCES target_types(id),
+	ADD CONSTRAINT like_type_id_like_types_id_fk
+		FOREIGN KEY (like_type_id) REFERENCES like_types(id);
+
+DESC posts;
+
+ALTER TABLE posts
+	ADD CONSTRAINT post_user_id_user_id_fk
+		FOREIGN KEY (user_id) REFERENCES users(id);
+	
+DESC media;
+
+ALTER TABLE media
+	ADD CONSTRAINT media_media_type_id_media_types_id_fk
+		FOREIGN KEY (media_type_id) REFERENCES media_types(id),
+	ADD CONSTRAINT media_user_id_user_id_fk
+		FOREIGN KEY (user_id) REFERENCES users(id);
+	
+	
+	
+DESC photo;
+
+ALTER TABLE photo
+	ADD CONSTRAINT photo_media_type_id_media_type_id_fk
+		FOREIGN KEY (media_type_id) REFERENCES media_types(id),
+	ADD CONSTRAINT photo_user_id_user_id_fk
+		FOREIGN KEY (user_id) REFERENCES users(id);
+	
+
+DESC relations;
+
+ALTER TABLE relations
+	ADD CONSTRAINT relations_user_id_user_id_fk
+		FOREIGN KEY (user_id) REFERENCES users(id),
+	ADD CONSTRAINT relative_id_user_id_fk
+		FOREIGN KEY (relative_id) REFERENCES users(id),
+	ADD CONSTRAINT relations_status_id_relation_statuses_fk
+		FOREIGN KEY (relation_status_id) REFERENCES relation_statuses(id);
+	
+
+
