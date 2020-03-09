04a4f3232e162b06d9ef600b0bae5625b806c5d3
antonbelov <mukha19@gmail.com>
2020-03-09 12:31:31 +0300

    Второе задание по первому уроку

---
+++ API_task2.py
@@ -0,0 +1,18 @@
+import requests
+import getpass
+from pprint import pprint
+import json
+
+main_link = 'https://api.github.com/user/repos' #Ссылка на репозитории
+
+username = input('Введите имя вашего пользователя!') #Запрос имени пользователя.
+password = getpass.getpass()  #Запрос пароля пользователя.
+
+r = requests.get(main_link, auth=(username, password))
+data = json.loads(r.text) #Получение данных о репозиториях пользователя.
+# pprint(data)
+
+#Вывод ссылок публичных репозиториев пользователя.
+for repo in data:
+    if not repo['private']:
+        print(repo['html_url'])
