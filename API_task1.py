617334fd3fa08c3f1ef41ed41a2d63201daa62e6
antonbelov <mukha19@gmail.com>
2020-03-09 12:31:02 +0300

    Первое задание по первому уроку

---
+++ API_task1.py
@@ -0,0 +1,16 @@
+import requests
+import getpass
+from pprint import pprint
+import json
+#Первое задание
+url = 'https://api.github.com'
+user='antonbelovalex'
+
+r = requests.get(f'{url}/users/{user}/repos')
+with open('data.json', 'w') as f:
+    json.dump(r.json(), f)
+
+for i in r.json():
+    print(i['name'])
+
+
